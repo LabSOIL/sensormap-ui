bb083a983c1edee52216e51341b91221b1e22a5a
Evan Thomas <evan.thomas@epfl.ch>
2024-06-27 15:45:51 +0200

    Revert code to original

--- src/maps/Areas.tsx
+++ src/maps/Areas.tsx
@@ -36,26 +36,20 @@ export const LocationFieldAreas = () => {
     // If there are no geometry coordinates, we can't display the area. There
     // needs to be at least one area record with valid geom to display the map.
     // Otherwise return a message
-    console.log("Areas:", areas);
     if (!areas.some(area => area["geom"] && area["geom"]["coordinates"])) {
         return <Typography variant="body">Add records to an area to display the map</Typography>;
     }
 
     const flipCoordinates = (coords) => {
-        console.log("Flip coordinates called:", coords);
         return coords.map(coord => [coord[1], coord[0]]);
     };
 
     const flipPolygonCoordinates = (polygon) => {
-        console.log("flipPolygonCoordinates called:", polygon);
         return polygon.map(ring => flipCoordinates(ring));
     };
     const validAreas = areas.filter(area => area["geom"] && area["geom"]["coordinates"]);
-    console.log("validAreas:", validAreas);
     const allCoordinates = validAreas.flatMap(area => flipPolygonCoordinates(area["geom"]["coordinates"])[0]);
-    console.log("allCoordinates:", allCoordinates);
     const bounds = L.latLngBounds(allCoordinates).pad(0.6); // Padding the bounds to zoom out further
-    console.log("Bounds:", bounds);
 
 
     return (
